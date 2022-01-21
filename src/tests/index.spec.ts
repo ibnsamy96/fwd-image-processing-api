d6af20eb616ef36736cad9a49c064e1d3f39915e
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-01-22 00:28:16 +0200

    added the index test

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -4,9 +4,31 @@ import app from '../index'
 // create a request object
 const request = supertest(app)
 
-describe('Test endpoint response', () => {
+describe("Test endpoint '/'", () => {
   it('test hello world endpoint', async () => {
     const response = await request.get('/')
     expect(response.status).toBe(200)
+  })
+})
+
+describe("Test endpoint response '/resize-image'", () => {
+  const endpoint = '/resize-image'
+
+  it('test ok', async () => {
+    const queriesLine = 'imageName=image1&width=100&height=500'
+    const response = await request.get(`${endpoint}?${queriesLine}`)
+    expect(response.status).toBe(200)
+  })
+
+  it('test bad requests due to string parameters', async () => {
+    const queriesLine = 'imageName=image1&width=100px&height=500px'
+    const response = await request.get(`${endpoint}?${queriesLine}`)
+    expect(response.status).toBe(400)
+  })
+
+  it('test not enough query parameters', async () => {
+    const queriesLine = 'imageName=image1j&width=100&height=500'
+    const response = await request.get(`${endpoint}?${queriesLine}`)
+    expect(response.status).toBe(404)
   })
 })
