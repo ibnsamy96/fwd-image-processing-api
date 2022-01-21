d7272dadb766751e6e00368dee3fcf6155a540c4
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-01-21 07:14:09 +0200

    fixed the resize function to work async

--- src/resize-image.ts
+++ src/resize-image.ts
@@ -1,20 +1,21 @@
 import sharp, { Sharp } from 'sharp'
 
-export const resizeImage = (
+export const resizeImage = async (
   imagePath: string,
   width: number,
   height: number,
   sourceDir: string,
   destinationDir: string
-) => {
+): Promise<void> => {
   console.log(imagePath)
 
   const [imageFileName, imageFileExtension] = imagePath.split('.')
 
   const imageSharpObject = sharp(`${sourceDir}/${imagePath}`)
-    .resize({ width, height })
-    .toFile(`${destinationDir}/${imageFileName}-${width}-${height}.${imageFileExtension}`)
-    .then((data) => {
-      console.log(data)
-    })
+  const resizedImageSharpObject = imageSharpObject.resize({ width, height })
+
+  const outputInfo = await resizedImageSharpObject.toFile(
+    `${destinationDir}/${imageFileName}-${width}-${height}.${imageFileExtension}`
+  )
+  console.log(outputInfo)
 }
