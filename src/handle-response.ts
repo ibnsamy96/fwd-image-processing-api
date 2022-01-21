b18ee872d048d7de695a966a1c2d0c5a37204d35
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2022-01-21 21:01:06 +0200

    updated handle-response module to increase readability

--- src/handle-response.ts
+++ src/handle-response.ts
@@ -3,10 +3,11 @@ import { StatusCodes } from 'http-status-codes'
 import { ResponseStatus } from './types/response.interface'
 
 export const errors: unknown = {
-  404: 'There is no image in the images folder with the name your specified.',
-  200: 'Request processing finished successfully.',
-  400: 'Make sure to define the needed width & height and for them to be numbers.',
-  500: 'An error happened while processing the image and it was logged, try again later.'
+  NOT_FOUND: 'There is no image in the images folder with the name your specified.',
+  OK: 'Request processing finished successfully.',
+  BAD_REQUEST: 'Make sure to define the needed width & height and for them to be numbers.',
+  INTERNAL_SERVER_ERROR:
+    'An error happened while processing the image and it was logged, try again later.'
 }
 
 type StatusConstantsType = keyof typeof StatusCodes
@@ -15,7 +16,7 @@ interface StatusConstantsTypeExtended extends StatusConstantsTypeFields {}
 
 export const getResponseStatus = (constant: StatusConstantsType): ResponseStatus => {
   const statusCode: number = (StatusCodes as StatusConstantsTypeExtended)[constant]
-  const statusMessage: string = (errors as keyof typeof errors)[statusCode]
+  const statusMessage: string = (errors as keyof typeof errors)[constant]
 
   return { code: statusCode, message: statusMessage }
 }
